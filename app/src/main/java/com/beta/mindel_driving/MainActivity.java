536b6c4ec18e889298e077ed8e68ca790d99c64c
Jerry <jerry@localhost.localdomain>
2023-05-11 09:16:40 -0100

    Initial commit

---
+++ app/src/main/java/com/beta/mindel_driving/MainActivity.java
@@ -0,0 +1,23 @@
+package com.beta.mindel_driving;
+
+import android.content.Intent;
+import android.graphics.Color;
+import android.os.Bundle;
+import android.os.Handler;
+import android.os.Looper;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+
+
+    }
+}
