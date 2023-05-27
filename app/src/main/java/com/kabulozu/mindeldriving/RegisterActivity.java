35ef43cbdc1902724f50d6041ec7e7cb3341489f
Péricles Borges <92488227+Jerry-523@users.noreply.github.com>
2023-05-27 20:37:07 +0000

    Add files via upload

---
+++ app/src/main/java/com/kabulozu/mindeldriving/RegisterActivity.java
@@ -0,0 +1,152 @@
+package com.kabulozu.mindeldriving;
+
+import android.annotation.SuppressLint;
+import android.content.Intent;
+import android.graphics.Color;
+import android.os.Bundle;
+import android.util.Log;
+import android.view.ActionMode;
+import android.view.View;
+import android.widget.TextView;
+import android.widget.Button;
+import android.widget.EditText;
+
+import androidx.appcompat.app.AppCompatActivity;
+import androidx.annotation.NonNull;
+
+import com.google.android.gms.tasks.OnCompleteListener;
+import com.google.android.gms.tasks.OnFailureListener;
+import com.google.android.gms.tasks.OnSuccessListener;
+import com.google.android.gms.tasks.Task;
+import com.google.android.material.snackbar.Snackbar;
+import com.google.firebase.auth.AuthResult;
+import com.google.firebase.auth.FirebaseAuth;
+import com.google.firebase.auth.FirebaseAuthInvalidCredentialsException;
+import com.google.firebase.auth.FirebaseAuthUserCollisionException;
+import com.google.firebase.auth.FirebaseAuthWeakPasswordException;
+import com.google.firebase.firestore.DocumentReference;
+import com.google.firebase.firestore.FirebaseFirestore;
+
+import java.util.HashMap;
+import java.util.Map;
+
+
+public class RegisterActivity extends AppCompatActivity {
+
+    TextView login;
+    EditText edit_nome, edit_email, edit_senha, edit_movel;
+    Button bt_cadastrar;
+    String[] mensagem = {"Preencha todos os campos", "Cadastro com sucesso"};
+    String usuarioID;
+
+
+
+
+    @SuppressLint("MissingInflatedId")
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_register);
+        if (getSupportActionBar() != null) {
+            getSupportActionBar().hide();
+        }
+        getWindow().setStatusBarColor(Color.parseColor("#FF000000"));
+        IniciarComponentes();
+
+        bt_cadastrar.setOnClickListener(new View.OnClickListener() {
+            @SuppressLint("ResourceAsColor")
+            @Override
+            public void onClick(View view) {
+                String nome = edit_nome.getText().toString();
+                String email = edit_email.getText().toString();
+                String senha = edit_senha.getText().toString();
+                String movel = edit_movel.getText().toString();
+
+                if (nome.isEmpty() || email.isEmpty() || senha.isEmpty()){
+                    Snackbar snackbar = Snackbar.make(view, mensagem[0], Snackbar.LENGTH_SHORT);
+                    snackbar.setBackgroundTint(Color.WHITE);
+                    snackbar.setTextColor(Color.BLACK);
+                    snackbar.show();
+                }else{
+                    CadastrarUsuario(view);
+                }
+            }
+        });
+        login = findViewById(R.id.login_here);
+
+        login.setOnClickListener(new View.OnClickListener(){
+            @Override
+            public void onClick(View view){
+                startActivity(new Intent(RegisterActivity.this,LoginActivity.class));
+            }
+        });
+    }
+
+
+
+    private void CadastrarUsuario(View view){
+        String email = edit_email.getText().toString();
+        String senha = edit_senha.getText().toString();
+
+        FirebaseAuth.getInstance().createUserWithEmailAndPassword(email, senha).addOnCompleteListener(new OnCompleteListener<AuthResult>(){
+            @Override
+            public void onComplete(@NonNull Task<AuthResult> task) {
+                if (task.isSuccessful()){
+                    SalvarDadosUsuario();
+                    Snackbar snackbar = Snackbar.make(view, mensagem[1],Snackbar.LENGTH_SHORT);
+                    snackbar.setBackgroundTint(Color.WHITE);
+                    snackbar.setTextColor(Color.BLACK);
+                    snackbar.show();
+                }else{
+                    String erro;
+                    try {
+                        throw task.getException();
+                    }catch (FirebaseAuthWeakPasswordException e){
+                        erro = "Digite uma senha com no minimo 6 caracteres";
+                    }catch (FirebaseAuthUserCollisionException e){
+                        erro = "Essa conta ja existe";
+                    }catch (FirebaseAuthInvalidCredentialsException e){
+                        erro = "E-mail Invalido";
+                    }catch (Exception e) {
+                        erro = "Erro ao Cadastrar";
+                    }
+                    Snackbar snackbar = Snackbar.make(view,erro,Snackbar.LENGTH_SHORT);
+                    snackbar.setBackgroundTint(Color.WHITE);
+                    snackbar.setTextColor(Color.BLACK);
+                    snackbar.show();
+                }
+            }
+        });
+    }
+    private void SalvarDadosUsuario(){
+        String nome = edit_nome.getText().toString();
+        String movel = edit_movel.getText().toString();
+        String email = edit_email.getText().toString();
+        FirebaseFirestore db = FirebaseFirestore.getInstance();
+        Map<String,Object> usuarios = new HashMap<>();
+        usuarios.put("nome",nome);
+        usuarios.put("telemovel", movel);
+        usuarios.put("email", email);
+        usuarioID = FirebaseAuth.getInstance().getCurrentUser().getUid();
+        DocumentReference documentReference = db.collection("Usuarios").document(usuarioID);
+        documentReference.set(usuarios).addOnSuccessListener(new OnSuccessListener<Void>() {
+            @Override
+            public void onSuccess(Void aVoid) {
+                Log.d("db", "Sucesso ao salvar dados");
+            }
+        })
+        .addOnFailureListener(new OnFailureListener() {
+            @Override
+            public void onFailure(@NonNull Exception e) {
+                Log.d("db_error", "Erro ao salvar os dados" + e.toString());
+            }
+        });
+    }
+    private void IniciarComponentes(){
+        edit_nome = findViewById(R.id.nomeRegister);
+        edit_email = findViewById(R.id.emailRegister);
+        edit_senha = findViewById(R.id.passwordRegister);
+        edit_movel = findViewById(R.id.movelRegister);
+        bt_cadastrar = findViewById(R.id.buttonRegister);
+    }
+}
