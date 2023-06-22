4b7bfd372899f0beaf9c4b63cd98609975a60b35
Péricles Borges <92488227+Jerry-523@users.noreply.github.com>
2023-06-22 00:42:17 +0000

    Add files via upload

--- app/src/main/java/com/kabulozu/mindeldriving/CustomerMapActivity.java
+++ app/src/main/java/com/kabulozu/mindeldriving/CustomerMapActivity.java
@@ -24,6 +24,8 @@ import androidx.drawerlayout.widget.DrawerLayout;
 
 import com.firebase.geofire.GeoFire;
 import com.firebase.geofire.GeoLocation;
+import com.firebase.geofire.GeoQuery;
+import com.firebase.geofire.GeoQueryEventListener;
 import com.google.android.gms.common.ConnectionResult;
 import com.google.android.gms.common.api.GoogleApiClient;
 import com.google.android.gms.location.FusedLocationProviderClient;
@@ -39,6 +41,7 @@ import com.google.android.gms.maps.model.LatLng;
 import com.google.android.gms.maps.model.MapStyleOptions;
 import com.google.android.gms.maps.model.MarkerOptions;
 import com.google.firebase.auth.FirebaseAuth;
+import com.google.firebase.database.DatabaseError;
 import com.google.firebase.database.DatabaseReference;
 import com.google.firebase.database.FirebaseDatabase;
 
@@ -100,8 +103,54 @@ public class CustomerMapActivity extends AppCompatActivity implements OnMapReady
 
                 pickupLocation = new LatLng(zLastLocation.getLatitude(), zLastLocation.getLongitude());
                 zMap.addMarker(new MarkerOptions().position(pickupLocation).title("Preciso de um Motorista"));
-                
+
                 mRequest.setText("Esperando motorista...");
+
+                getClosestDriver();
+            }
+        });
+    }
+    private int radius = 1;
+    private  Boolean driverFound = false;
+    private String driverFoundID;
+    private void getClosestDriver(){
+        DatabaseReference driverLocation = FirebaseDatabase.getInstance().getReference().child("DriverAvailable");
+
+        GeoFire geoFire = new GeoFire(driverLocation);
+        GeoQuery geoQuery = geoFire.queryAtLocation(new GeoLocation(pickupLocation.latitude, pickupLocation.longitude), radius);
+        geoQuery.removeAllListeners();
+
+        geoQuery.addGeoQueryEventListener(new GeoQueryEventListener() {
+            @Override
+            public void onKeyEntered(String key, GeoLocation location) {
+                if(!driverFound){
+                    driverFound = true;
+                    driverFoundID = key;
+                }
+
+            }
+
+            @Override
+            public void onKeyExited(String key) {
+
+            }
+
+            @Override
+            public void onKeyMoved(String key, GeoLocation location) {
+
+            }
+
+            @Override
+            public void onGeoQueryReady() {
+                if (!driverFound){
+                    radius++;
+                    getClosestDriver();
+                }
+            }
+
+            @Override
+            public void onGeoQueryError(DatabaseError error) {
+
             }
         });
     }
